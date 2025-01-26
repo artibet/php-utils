dbcadc5dc4bdf779759e5824d9322d66e03500c7
Tsakiridis Sotiris <artibetgr@gmail.com>
2025-01-26 20:46:37 +0200

    Fix bug

--- src/Tables.php
+++ src/Tables.php
@@ -8,93 +8,93 @@ namespace Artibet\PhpUtils;
 
 class Tables
 {
-	// ---------------------------------------------------------------------------------------
-	// Table row to split pages
-	// $tds is an array of table cells:
-	// $tds = [
-	// 		'text' => '.....'
-	//		'style' => '....'
-	// ]
-	// ---------------------------------------------------------------------------------------
-	public static function tableRow($tds)
-	{
-		// The row to be built
-		$row = '';
+  // ---------------------------------------------------------------------------------------
+  // Table row to split pages
+  // $tds is an array of table cells:
+  // $tds = [
+  // 		'text' => '.....'
+  //		'style' => '....'
+  // ]
+  // ---------------------------------------------------------------------------------------
+  public static function tableRow($tds)
+  {
+    // The row to be built
+    $row = '';
 
-		// Split td data into rows
-		$lines = [];
-		foreach ($tds as $td) {
-			$lines[] = explode("\n", str_replace("\t", " ", $td['text']));
-		}
+    // Split td data into rows
+    $lines = [];
+    foreach ($tds as $td) {
+      $lines[] = explode("\n", str_replace("\t", " ", $td['text']));
+    }
 
-		// Μέγιστο πλήθος γραμμών
-		$maxLines = 0;
-		for ($i = 0; $i < count($lines); $i++) {
-			$c = count($lines[$i]);
-			if ($c > $maxLines) $maxLines = $c;
-		}
+    // Μέγιστο πλήθος γραμμών
+    $maxLines = 0;
+    for ($i = 0; $i < count($lines); $i++) {
+      $c = count($lines[$i]);
+      if ($c > $maxLines) $maxLines = $c;
+    }
 
-		// Δημιουργία tr για κάθε γραμμή
-		for ($i = 0; $i < $maxLines; $i++) {
+    // Δημιουργία tr για κάθε γραμμή
+    for ($i = 0; $i < $maxLines; $i++) {
 
-			// Μία μοναδική γραμμή
-			if ($i == 0 && $maxLines == 1) {
-				$row = "<tr>";
-				for ($k = 0; $k < count($tds); $k++) {
-					$style = array_key_exists('style', $tds[$k]) ? $tds[$k]['style'] : '';
-					if (count($lines[$k]) > 0) {
-						$row .= "<td style='border: 1px solid black; $style>" . $lines[$k][0] . "</td>";
-					} else {
-						$row .= "<td style='border: 1px solid black; $style'></td>";
-					}
-				}
-				$row .= "</tr>";
-			}
+      // Μία μοναδική γραμμή
+      if ($i == 0 && $maxLines == 1) {
+        $row = "<tr>";
+        for ($k = 0; $k < count($tds); $k++) {
+          $style = array_key_exists('style', $tds[$k]) ? $tds[$k]['style'] : '';
+          if (count($lines[$k]) > 0) {
+            $row .= "<td style='border: 1px solid black; $style'>" . $lines[$k][0] . "</td>";
+          } else {
+            $row .= "<td style='border: 1px solid black; $style'></td>";
+          }
+        }
+        $row .= "</tr>";
+      }
 
-			// Πρώτη γραμμή - υπάρχουν και άλλες
-			else if ($i == 0 && $maxLines > 1) {
-				$row = "<tr>";
-				for ($k = 0; $k < count($tds); $k++) {
-					$style = array_key_exists('style', $tds[$k]) ? $tds[$k]['style'] : '';
-					if (count($lines[$k]) > 0) {
-						$row .= "<td style='border-left: 1px solid black; border-top: 1px solid black; border-right: 1px solid black; $style'>" . $lines[$k][0] . "</td>";
-					} else {
-						$row .= "<td style='border-left: 1px solid black; border-top: 1px solid black; border-right: 1px solid black; $style'></td>";
-					}
-				}
-				$row .= "</tr>";
-			}
+      // Πρώτη γραμμή - υπάρχουν και άλλες
+      else if ($i == 0 && $maxLines > 1) {
+        $row = "<tr>";
+        for ($k = 0; $k < count($tds); $k++) {
+          $style = array_key_exists('style', $tds[$k]) ? $tds[$k]['style'] : '';
+          if (count($lines[$k]) > 0) {
+            $row .= "<td style='border-left: 1px solid black; border-top: 1px solid black; border-right: 1px solid black; $style'>" . $lines[$k][0] . "</td>";
+          } else {
+            $row .= "<td style='border-left: 1px solid black; border-top: 1px solid black; border-right: 1px solid black; $style'></td>";
+          }
+        }
+        $row .= "</tr>";
+      }
 
-			// Υπόλοιπες γραμμές εκτός από την τελευταία
-			else if ($i > 0 && $i < $maxLines - 1) {
-				$row .= "<tr>";
-				for ($k = 0; $k < count($tds); $k++) {
-					$style = array_key_exists('style', $tds[$k]) ? $tds[$k]['style'] : '';
-					if (count($lines[$k]) >= $i + 1) {
-						$row .= "<td style='border-left: 1px solid black; border-right: 1px solid black; $style'>" . $lines[$k][$i] . "</td>";
-					} else {
-						$row .= "<td style='border-left: 1px solid black; border-right: 1px solid black; $style'></td>";
-					}
-				}
-				$row .= "</tr>";
-			}
+      // Υπόλοιπες γραμμές εκτός από την τελευταία
+      else if ($i > 0 && $i < $maxLines - 1) {
+        $row .= "<tr>";
+        for ($k = 0; $k < count($tds); $k++) {
+          $style = array_key_exists('style', $tds[$k]) ? $tds[$k]['style'] : '';
+          if (count($lines[$k]) >= $i + 1) {
+            $row .= "<td style='border-left: 1px solid black; border-right: 1px solid black; $style'>" . $lines[$k][$i] . "</td>";
+          } else {
+            $row .= "<td style='border-left: 1px solid black; border-right: 1px solid black; $style'></td>";
+          }
+        }
+        $row .= "</tr>";
+      }
 
-			// Η τελευταία γραμμή
-			else if ($i == $maxLines - 1) {
-				$row .= "<tr>";
-				for ($k = 0; $k < count($tds); $k++) {
-					$style = array_key_exists('style', $tds[$k]) ? $tds[$k]['style'] : '';
-					if (count($lines[$k]) == $maxLines) {
-						$row .= "<td style='border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black; $style'>" . $lines[$k][$maxLines - 1] . "</td>";
-					} else {
-						$row .= "<td style='border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black; $style'></td>";
-					}
-				}
-				$row .= "</tr>";
-			}
-		}
+      // Η τελευταία γραμμή
+      else if ($i == $maxLines - 1) {
+        $row .= "<tr>";
+        for ($k = 0; $k < count($tds); $k++) {
+          $style = array_key_exists('style', $tds[$k]) ? $tds[$k]['style'] : '';
+          if (count($lines[$k]) == $maxLines) {
+            $row .= "<td style='border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black; $style'>" . $lines[$k][$maxLines - 1] . "</td>";
+          } else {
+            $row .= "<td style='border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black; $style'></td>";
+          }
+        }
+        $row .= "</tr>";
+      }
+    }
 
-		// Επιστροφή της γραμμής
-		return $row;
-	}
+    // Επιστροφή της γραμμής
+    return $row;
+  }
 }
